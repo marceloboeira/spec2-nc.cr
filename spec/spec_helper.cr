98953ce7186b3836b6592920900cfdaf83cb01d8
Marcelo Boeira <me@marceloboeira.com>
2016-02-10 02:30:33 -0200

    feature: Add basic implementation

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,6 @@
 require "spec2"
 require "../src/spec2-nc"
+
+include Spec2::GlobalDSL
+
+Spec2.configure_reporter(Spec2::Reporters::Nc)
--- src/spec2-nc.cr
+++ src/spec2-nc.cr
@@ -1,17 +1,38 @@
 require "spec2"
+require "terminal-notifier"
 
 module Spec2
   module Reporters
     class Nc < Reporter
-      def initialize; end
+      def initialize
+        @errors = [] of Spec2::ExpectationNotMet
+      end
+
       def configure_output(@output); end
       def context_started(context); end
       def context_finished(context); end
       def example_started(example); end
       def example_succeeded(example); end
-      def example_failed(example, exception); end
-      def example_errored(example, exception); end
-      def report; end
+
+      def example_failed(example, exception)
+        @errors << exception
+      end
+
+      def example_errored(example, exception)
+        @errors << exception
+      end
+
+      def report
+        if @errors.size > 0
+          notify("Error")
+        else
+          notify("Success")
+        end
+      end
+
+      private def notify(message : String)
+        TerminalNotifier.notify(message)
+      end
     end
   end
 end
