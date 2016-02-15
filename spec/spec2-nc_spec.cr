05fd493b19bf94f07777a4745925cd2bdb42362a
Marcelo Boeira <me@marceloboeira.com>
2016-02-14 23:33:03 -0200

    temp: Mock Terminal Notifier

--- spec/spec2-nc_spec.cr
+++ spec/spec2-nc_spec.cr
@@ -2,6 +2,10 @@ require "./spec_helper"
 
 create_double "Example" { }
 
+create_module_mock TerminalNotifier do
+  mock self.notify(message)
+end
+
 describe Spec2::Reporters::Nc do
   let(reporter) { Spec2::Reporters::Nc.new }
   let(example) { double("Example") }
@@ -12,7 +16,12 @@ describe Spec2::Reporters::Nc do
     end
 
     it "notifies with error message" do
+      message = "#{Spec2::Reporters::Nc::ERROR_ICON} Error"
+      allow(TerminalNotifier).to receive(self.notify(message)).and_return(nil)
+
       reporter.report
+
+      expect(TerminalNotifier).to have_received(self.notify(message))
     end
   end
 
@@ -22,7 +31,12 @@ describe Spec2::Reporters::Nc do
     end
 
     it "notifies with success message" do
+      message = "#{Spec2::Reporters::Nc::SUCCESS_ICON} Success"
+      allow(TerminalNotifier).to receive(self.notify(message)).and_return(nil)
+
       reporter.report
+
+      expect(TerminalNotifier).to have_received(self.notify(message))
     end
   end
 end
