633b5ff2f1248aad049806b0b42c1b614273d5b3
Marcelo Boeira <me@marceloboeira.com>
2016-02-15 22:51:52 -0200

    test: Add basic test structure

--- spec/spec2-nc_spec.cr
+++ spec/spec2-nc_spec.cr
@@ -1,5 +1,28 @@
 require "./spec_helper"
 
-describe Spec2::Reporters::Nc do
+create_double "Example" { }
 
+describe Spec2::Reporters::Nc do
+  let(reporter) { Spec2::Reporters::Nc.new }
+  let(example) { double("Example") }
+
+  context "with failed examples" do
+    before do
+      reporter.example_failed(example, Spec2::ExpectationNotMet.new("Default Error"))
+    end
+
+    it "notifies with error message" do
+      reporter.report
+    end
+  end
+
+  context "with succesfull examples" do
+    before do
+      reporter.example_succeeded(example)
+    end
+
+    it "notifies with success message" do
+      reporter.report
+    end
+  end
 end
